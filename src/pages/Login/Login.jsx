2f461ef7cd04ce24faf5310050e902bd46aba97a
Yến Nhi <nhanonggia@gmail.com>
2025-02-23 18:25:23 +0700

    login ui

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,43 @@
-import { Button, Empty } from "antd";
+import { Button, Form, Input } from "antd";
 
 const Login = () => {
   return (
-    <div>
-      <Button type="primary" className="m-3">
-        Click me
-      </Button>
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div className="mb-8">
+        <img src="/logo.svg" alt="Logo" className="h-10" />
+      </div>
 
-      <Empty />
+      <Form layout="vertical" className="w-[300px]">
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng nhập username",
+            },
+          ]}
+        >
+          <Input placeholder="Nhập username" />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng nhập mật khẩu",
+            },
+          ]}
+        >
+          <Input.Password placeholder="Nhập mật khẩu" />
+        </Form.Item>
+
+        <Button type="primary" className="w-full">
+          Login
+        </Button>
+      </Form>
     </div>
   );
 };
