7563da170048abbf7b21a3f2991d8c1d00a4f91a
Yến Nhi <nhanonggia@gmail.com>
2025-02-24 20:13:01 +0700

    update ui

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,13 @@ import { Button, Form, Input } from "antd";
 
 const Login = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div className="h-screen flex flex-col items-center justify-center relative overflow-hidden">
+      <div className="absolute w-0.5 bg-black h-screen -rotate-45 -top-[16%] right-0"></div>
+      <div className="absolute w-2.5 bg-black h-screen -rotate-45 -top-[24%] right-[10%]"></div>
+
+      <div className="absolute w-0.5 bg-black h-screen -rotate-45 -bottom-[16%] left-0"></div>
+      <div className="absolute w-2.5 bg-black h-screen -rotate-45 -bottom-[24%] left-[10%]"></div>
+
       <div className="mb-8">
         <img src="/logo.svg" alt="Logo" className="h-10" />
       </div>
