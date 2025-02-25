25bdacd1fe7756b84a3240edf241ddb1791f92bd
Yến Nhi <nhanonggia@gmail.com>
2025-02-25 14:59:27 +0700

    update login ui

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,3 @@
-import { Button, Form, Input } from "antd";
-
 const Login = () => {
   return (
     <div className="h-screen flex flex-col items-center justify-center relative overflow-hidden">
@@ -10,40 +8,41 @@ const Login = () => {
       <div className="absolute w-2.5 bg-black h-screen -rotate-45 -bottom-[24%] left-[10%]"></div>
 
       <div className="mb-8">
-        <img src="/logo.svg" alt="Logo" className="h-10" />
+        <img src="/logo.svg" alt="Logo" className="h-[60px]" />
       </div>
 
-      <Form layout="vertical" className="w-[300px]">
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: "Vui lòng nhập username",
-            },
-          ]}
-        >
-          <Input placeholder="Nhập username" />
-        </Form.Item>
+      <form action="" className="w-[300px]">
+        <div className="mb-5">
+          <div className="border rounded-sm h-[45px] px-3 flex items-center gap-x-3">
+            <img src="/icons/user.svg" alt="Icon" />
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Vui lòng nhập mật khẩu",
-            },
-          ]}
-        >
-          <Input.Password placeholder="Nhập mật khẩu" />
-        </Form.Item>
+            <input
+              type="text"
+              placeholder="USERNAME"
+              className="h-full w-full outline-none placeholder:text-black"
+            />
+          </div>
+        </div>
 
-        <Button type="primary" className="w-full">
-          Login
-        </Button>
-      </Form>
+        <div>
+          <div className="border rounded-sm h-[45px] px-3 flex items-center gap-x-3">
+            <img src="/icons/lock.svg" alt="Icon" />
+
+            <input
+              type="password"
+              placeholder="PASSWORD"
+              className="h-full w-full outline-none placeholder:text-black"
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-black w-full h-[45px] rounded-sm mt-11 text-white cursor-pointer"
+        >
+          LOGIN
+        </button>
+      </form>
     </div>
   );
 };
